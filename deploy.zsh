46ca771c47cbbdc56900b64bb2df70f5fd7a2605
gactocat <gactocat@gmail.com>
2022-06-19 16:36:21 +0900

    fix deploy.zsh

--- deploy.zsh
+++ deploy.zsh
@@ -11,16 +11,18 @@ fi
 
 cd "$DOTPATH" || exit 1
 
-mkdir -p "$HOME/.config/{gitui,nvim/lua,zsh}"
 
-ln -fvns $DOTPATH/.config/gitui/key_bindings.ron $HOME/.config/gitui/key_bindings.ron
+mkdir -p "$HOME/.config/gitui"
+ln -fvns "$DOTPATH/.config/gitui/key_bindings.ron" "$HOME/.config/gitui/key_bindings.ron"
 
-ln -fvns $DOTPATH/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -fvns $DOTPATH/.config/nvim/lua/colors.lua $HOME/.config/nvim/lua/colors.lua
-ln -fvns $DOTPATH/.config/nvim/lua/keymaps.lua $HOME/.config/nvim/lua/keymaps.lua
-ln -fvns $DOTPATH/.config/nvim/lua/plugins.lua $HOME/.config/nvim/lua/plugins.lua
+mkdir -p "$HOME/.config/nvim/lua"
+ln -fvns "$DOTPATH/.config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -fvns "$DOTPATH/.config/nvim/lua/colors.lua" "$HOME/.config/nvim/lua/colors.lua"
+ln -fvns "$DOTPATH/.config/nvim/lua/keymaps.lua" "$HOME/.config/nvim/lua/keymaps.lua"
+ln -fvns "$DOTPATH/.config/nvim/lua/plugins.lua" "$HOME/.config/nvim/lua/plugins.lua"
 
-ln -fvns $DOTPATH/.config/zsh/.fzf.zsh $HOME/.config/zsh/.fzf.zsh
-ln -fvns $DOTPATH/.config/zsh/.zimrc.zsh $HOME/.config/zsh/.zimrc.zsh
-ln -fvns $DOTPATH/.config/zsh/.zshrc.zsh $HOME/.config/zsh/.zshrc.zsh
+mkdir -p "$HOME/.config/zsh"
+ln -fvns "$DOTPATH/.config/zsh/.fzf.zsh" "$HOME/.config/zsh/.fzf.zsh"
+ln -fvns "$DOTPATH/.config/zsh/.zimrc.zsh" "$HOME/.config/zsh/.zimrc.zsh"
+ln -fvns "$DOTPATH/.config/zsh/.zshrc.zsh" "$HOME/.config/zsh/.zshrc.zsh"
 
